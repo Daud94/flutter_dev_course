402007a8e0a9d0000a009ff85a87cf8eaa536e51
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-10-02 22:32:48 +0100

    adding Card, padding, listtile

--- Micard/lib/main.dart
+++ Micard/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'main.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -11,30 +13,68 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: SafeArea(
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('images/self.jpg'),
-                ),
-                Text('Daud Olarewaju',
-                  style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Pacifico'
-                  ),),
-                Text('FLUTTER DEVELOPER', style: TextStyle(
-                  fontFamily: 'SourceSansPro',
-                  color: Colors.teal.shade100,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold
-                ),)
-              ],
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/self.jpg'),
             ),
-          ),
-        ));
+            Text(
+              'Daud Olarewaju',
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'),
+            ),
+            Text('FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 20, width: 150,
+              child: Divider(
+                color: Colors.teal.shade100 ,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.phone, color: Colors.teal.shade900),
+                title: Text(
+                  '+234 8062 9641 37',
+                  style: TextStyle(
+                    fontFamily: 'SourceSanspro',
+                    color: Colors.teal.shade900,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.email, color: Colors.teal.shade900),
+                title: Text(
+                  'olarewajuyahydaud@gmail.com',
+                  style: TextStyle(
+                    fontFamily: 'SourceSanspro',
+                    color: Colors.teal.shade900,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
