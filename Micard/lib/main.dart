03f3baa81784fd3bb5bd714fe1bfccc0105c5c49
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-10-02 17:23:24 +0100

    adding custom font

--- Micard/lib/main.dart
+++ Micard/lib/main.dart
@@ -11,23 +11,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('images/self.jpg'),
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/self.jpg'),
+                ),
+                Text('Daud Olarewaju',
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pacifico'
+                  ),),
+                Text('FLUTTER DEVELOPER', style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold
+                ),)
+              ],
             ),
-            Text('Daud Olarewaju',
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),)
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 }
