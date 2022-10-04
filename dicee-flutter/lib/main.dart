e872c16d493b923ced1164118905de40a103544e
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-10-04 13:28:45 +0100

    Stateul Widget, math lib, randomizing

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +17,16 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,15 +34,24 @@ class DicePage extends StatelessWidget {
         Expanded(
           flex: 1,
           child: TextButton(
-            child: Image.asset('images/dice1.png'),
+            child: Image.asset('images/dice$leftDiceNumber.png'),
             onPressed: () {
-
+              setState(() {
+                leftDiceNumber = Random().nextInt(6) + 1;
+              });
             },
           ),
         ),
         Expanded(
           flex: 1,
-          child: TextButton(child: Image.asset('images/dice1.png')),
+          child: TextButton(
+            child: Image.asset('images/dice$rightDiceNumber.png'),
+            onPressed: () {
+              setState(() {
+                rightDiceNumber = Random().nextInt(6) + 1;
+              });
+            },
+          ),
         ),
       ]),
     );
