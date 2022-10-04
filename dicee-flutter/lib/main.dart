d702e445f65929ba5188229aee9155bac8cfef9b
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-10-04 13:53:45 +0100

    refractoring

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -27,6 +27,14 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
+  void throwDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;
+      rightDiceNumber = Random().nextInt(6)+1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -36,9 +44,7 @@ class _DicePageState extends State<DicePage> {
           child: TextButton(
             child: Image.asset('images/dice$leftDiceNumber.png'),
             onPressed: () {
-              setState(() {
-                leftDiceNumber = Random().nextInt(6) + 1;
-              });
+              throwDice();
             },
           ),
         ),
@@ -47,9 +53,7 @@ class _DicePageState extends State<DicePage> {
           child: TextButton(
             child: Image.asset('images/dice$rightDiceNumber.png'),
             onPressed: () {
-              setState(() {
-                rightDiceNumber = Random().nextInt(6) + 1;
-              });
+              throwDice();
             },
           ),
         ),
