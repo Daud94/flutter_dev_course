930c261d81284fc2d13670a6e96f040256c91877
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-10-08 13:09:24 +0100

    classes

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler_flutter/question.dart';
 
 void main() {
-  runApp(const Quizzler());
+  runApp(Quizzler());
 }
 
-class Quizzler extends StatelessWidget {
+class Quizzler extends StatefulWidget {
   const Quizzler({Key? key}) : super(key: key);
+
+  @override
+  State<Quizzler> createState() => _QuizzlerState();
+}
+
+class _QuizzlerState extends State<Quizzler> {
+  // list to keep track of answered questions
+  List<Widget> scoreKeeper = [];
+
+  // list of questions
+  List<Question> questions = [
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true)
+  ];
+
+  int questionNo = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,52 +37,90 @@ class Quizzler extends StatelessWidget {
               Expanded(
                 flex: 5,
                 child: Center(
-                  child: Text(
-                    'This is where the text will be',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      questions[questionNo].questionText,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-              // First Button
+              // True Button
               Expanded(
                   child: Padding(
                 padding: const EdgeInsets.all(15.0),
                 child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'True',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  style: ButtonStyle(
+                  onPressed: () {
+                    setState(() {
+                      bool correctAnswer = questions[questionNo].questionAnswer;
+                      if (correctAnswer == true) {
+                        scoreKeeper.add(Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        ));
+                      } else {
+                        scoreKeeper.add(Icon(
+                          Icons.check,
+                          color: Colors.red,
+                        ));
+                      }
+                    });
+                    questionNo++;
+                  },
+                  style: const ButtonStyle(
                     backgroundColor:
                         MaterialStatePropertyAll<Color>(Colors.green),
                   ),
+                  child: const Text(
+                    'True',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               )),
-              // Seconf button
+              // False button
               Expanded(
                   child: Padding(
                 padding: const EdgeInsets.all(15.0),
                 child: TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        bool correctAnswer =
+                            questions[questionNo].questionAnswer;
+                        if (correctAnswer == false) {
+                          scoreKeeper.add(Icon(
+                            Icons.check,
+                            color: Colors.green,
+                          ));
+                        } else {
+                          scoreKeeper.add(Icon(
+                            Icons.check,
+                            color: Colors.red,
+                          ));
+                        }
+                      });
+                      questionNo++;
+                    },
                     style: TextButton.styleFrom(
                       backgroundColor: Colors.red,
                     ),
-                    child: Text(
+                    child: const Text(
                       'False',
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: 20.0,
                           fontWeight: FontWeight.bold),
                     )),
-              ))
+              )),
+              Row(
+                children: scoreKeeper,
+              )
             ],
           ),
         ),
@@ -72,3 +128,9 @@ class Quizzler extends StatelessWidget {
     );
   }
 }
+
+/*
+question1: , false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
